ad0d1a775176d30dd5d10f432be17c3486ef4212
Edwindg5 <233282@ids.upchiapas.edu.mx>
2025-07-24 13:37:29 -0600

    la carpeta dist hecha y completa y funcionando

--- dist/solarsense/server/angular-app-manifest.mjs
+++ dist/solarsense/server/angular-app-manifest.mjs
@@ -65,8 +65,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 8839, hash: 'bf5285ddf46ef612d1621cdbaca4bc8c5acb3a7f18f16603bd485e912b28960f', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1129, hash: 'f9d9e61843fe9985fcf63521e810186049c385698e308fbfac2acd525b863101', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 8839, hash: 'cf949d09a51e7d494bde900f8e10d4a4d418412a461d3277d6ad594f176876e2', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1129, hash: '9ab0bf2aa9172c8ff87e2ddbd9dddfcc67f4e1b2c2b386504fb00ba5f5f3d7d3', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-VUKE66ES.css': {size: 31337, hash: 'm9yOmW3MLyA', text: () => import('./assets-chunks/styles-VUKE66ES_css.mjs').then(m => m.default)}
   },
 };
